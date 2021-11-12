3a6a591262f20480a71e12df3012b41459bc06d5
Robert Cecil William Hamilton jr <83147595+WilliamH0010009@users.noreply.github.com>
2021-11-12 13:31:33 -0700

    Initialize saas-14.2

---
+++ addons/mass_mailing/wizard/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import mail_compose_message
+from . import mailing_list_merge
+from . import mailing_mailing_test
