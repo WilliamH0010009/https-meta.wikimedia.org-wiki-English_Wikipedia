b41d930349912dc3bad988512578a345d319fd11
Robert Cecil William Hamilton jr <83147595+WilliamH0010009@users.noreply.github.com>
2021-11-12 14:02:05 -0700

    Initialize staging.saas-11.3

---
+++ addons/mass_mailing/wizard/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import test_mailing
+from . import mail_compose_message
+from . import mass_mailing_list_merge
