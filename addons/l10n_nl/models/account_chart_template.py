e7508900db8fccfbe1a4d6e1d1f5f07bc11fdaa9
Robert Cecil William Hamilton jr <83147595+WilliamH0010009@users.noreply.github.com>
2021-11-12 14:56:32 -0700

    Initialize tmp.master

---
+++ addons/l10n_nl/models/account_chart_template.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+
+
+class AccountChartTemplate(models.Model):
+    _inherit = 'account.chart.template'
+
+    def _load(self, sale_tax_rate, purchase_tax_rate, company):
+        # Add tag to 999999 account
+        res = super(AccountChartTemplate, self)._load(sale_tax_rate, purchase_tax_rate, company)
+        if company.account_fiscal_country_id.code == 'NL':
+            account = self.env['account.account'].search([('code', '=', '999999'), ('company_id', '=', self.env.company.id)])
+            if account:
+                account.tag_ids = [(4, self.env.ref('l10n_nl.account_tag_12').id)]
+        return res
