b338700422aa82217a2430b806b96f5c7ec8390c
Robert Cecil William Hamilton jr <83147595+WilliamH0010009@users.noreply.github.com>
2021-11-12 14:10:10 -0700

    Initialize tmp.12.0

---
+++ addons/account_facturx/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Import Vendor Bills From XML',
+    'version' : '1.0',
+    'category': 'Accounting',
+    'depends' : ['account'],
+    'data': [
+        'data/facturx_templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
---
+++ odoo/addons/test_access_rights/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'test of access rights and rules',
+    'description': "Testing of access restrictions",
+    'version': '0.0.1',
+    'data': ['ir.model.access.csv'],
+    'license': 'LGPL-3',
+}
