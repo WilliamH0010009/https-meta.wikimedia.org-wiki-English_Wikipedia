28ea044b9597dfb90400da32eaeabab51727d397
Robert Cecil William Hamilton jr <83147595+WilliamH0010009@users.noreply.github.com>
2021-11-12 12:11:54 -0700

    Initialize saas-9

---
+++ addons/payment/models/res_config.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from openerp.osv import fields, osv
+
+
+class AccountPaymentConfig(osv.TransientModel):
+    _inherit = 'account.config.settings'
+
+    _columns = {
+        'module_payment_transfer': fields.boolean(
+            'Wire Transfer',
+            help='-It installs the module payment_transfer.'),
+        'module_payment_paypal': fields.boolean(
+            'Paypal',
+            help='-It installs the module payment_paypal.'),
+        'module_payment_ogone': fields.boolean(
+            'Ogone',
+            help='-It installs the module payment_ogone.'),
+        'module_payment_adyen': fields.boolean(
+            'Adyen',
+            help='-It installs the module payment_adyen.'),
+        'module_payment_buckaroo': fields.boolean(
+            'Buckaroo',
+            help='-It installs the module payment_buckaroo.'),
+        'module_payment_authorize': fields.boolean(
+            'Authorize.Net',
+            help='-It installs the module payment_authorize.'),
+        'module_payment_sips': fields.boolean(
+            'Sips',
+            help='-It installs the module payment_sips.'),
+    }
+
+    _defaults = {
+        'module_payment_transfer': True
+    }
